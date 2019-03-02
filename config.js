76da3a7d1aa61da3124183e2ff1cb8e3546add6f
Zoljargal Fallows <zol.fallows@hotmail.com>
2019-03-02 22:41:23 +0000

    updated config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 export default {
-    API_ENDPOINT: `https://...............herokuapp.com/`,
+    API_ENDPOINT: `http://localhost:8000/api/noteful/`,
     API_KEY: process.env.REACT_APP_API_KEY,
   }
